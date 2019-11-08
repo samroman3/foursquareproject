587c2c2a5c7c4ef5b9d9a254ae134d2842b5d755
Sam <samroman3@gmail.com>
2019-11-08 17:56:04 -0500

    Adds viewcontroller to create new collection

--- foursquare/foursquare/View Controllers/BookmarksViewController.swift
+++ foursquare/foursquare/View Controllers/BookmarksViewController.swift
@@ -44,5 +44,9 @@ extension BookmarksViewController: UICollectionViewDelegate, UICollectionViewDel
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 194, height: 228)
+    }
+    
     
 }
